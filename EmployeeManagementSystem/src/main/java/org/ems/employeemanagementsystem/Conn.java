4334a747676b4ce8e6c5dfd0560cdd866b5e3747
Arun Pandey <128284476+arunpandeyg@users.noreply.github.com>
2026-08-02 17:25:48 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/src/main/java/org/ems/employeemanagementsystem/Conn.java
@@ -0,0 +1,18 @@
+package org.ems.employeemanagementsystem;
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "codeforinterview");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
